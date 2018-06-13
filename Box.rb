9a0291e06f50ff7115862b73d6772b5d2474313a
Любовь Онуфриюк <lika.lex555@gmail.com>
2018-06-13 12:32:14 +0300

    Box

---
+++ Box.rb
@@ -0,0 +1,23 @@
+#Given a number as a parameter, return an array containing strings which form a box. Like this:
+n = 5
+[
+  '-----',
+  '-   -',
+  '-   -',
+  '-   -',
+  '-----'
+]
+
+n = 3
+[
+  '---',
+  '- -',
+  '---'
+]
+
+def box(n)
+  result = []
+  result << "-" * n
+  (2..n-1).each {|x| result << "-" + (" " * (n-2)) + "-" }
+  result << "-" * n
+end
